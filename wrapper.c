ec33d8817fbbf0497c83c72dbadf654f0dd41d52
Yicheng Li <yli137@vols.utk.edu>
2025-01-06 15:51:50 -0500

    removed size check and it worked

--- wrapper.c
+++ wrapper.c
@@ -60,12 +60,45 @@ int wrapper_MPI_Isend( void *buf, int count, MPI_Datatype type, int dest,
 			pthread_mutex_lock(&(pair[index].pair_lock));
 			pair[index].ncomp = 0;
 			
-			if(pair[index].comp_size < type_size){
-				//printf("%d send index %d ready %d comp_size %d type_size %d pair_size %d\n",
-				//		rank, index, pair[index].ready, pair[index].comp_size, type_size, pair_size);
-				
+			//printf("%d send index %d ready %d comp_size %d type_size %d pair_size %d\n",
+			//		rank, index, pair[index].ready, pair[index].comp_size, type_size, pair_size);
+
 #if 0
-				pair[index].comp_time = MPI_Wtime();
+			pair[index].comp_time = MPI_Wtime();
+			int comp_size = compress_lz4_buffer(pair[index].isend_addr, pair[index].isend_size,
+					pair[index].comp_addr, pair[index].comp_size);
+
+			if(comp_size < pair[index].isend_size && comp_size != 0){
+				pair[index].comp_size = comp_size;
+				pair[index].ready = 1;
+				pair[index].thread = 0;
+			}
+#endif
+			if(pair[index].ready == 1 && pair[index].comp_size != 0){
+				//printf("%d send index %d comp_size %d type_size %d\n",
+				//		rank, index, pair[index].comp_size, type_size);
+
+#if 0
+				printf("%d send index %d comp_size %d type_size %d pair_size %d comp_time %.3f send_time %.3f last_fault %.9f difference %.9f gap %.9f thread %d faults %d\n",
+						rank, index, pair[index].comp_size, type_size, pair_size,
+						pair[index].comp_time, pair[index].send_time, 
+						pair[index].last_fault,
+						pair[index].send_time - pair[index].comp_time,
+						pair[index].send_time - pair[index].last_fault,
+						pair[index].thread, pair[index].faults);
+#endif
+
+				//if(rank == 0)
+				//printf("rank %d send i %d pair_size %d\n", rank, index, pair_size);
+
+				int comp_ret = MPI_Isend(pair[index].comp_addr, pair[index].comp_size, MPI_BYTE,
+						dest, tag, comm, request);
+				pair[index].faults = 0;
+				pthread_mutex_unlock(&(pair[index].pair_lock));
+				return comp_ret;
+			}
+			else if(pair[index].ready != 1){
+				pair[index].ready == 1;
 				int comp_size = compress_lz4_buffer(pair[index].isend_addr, pair[index].isend_size,
 						pair[index].comp_addr, pair[index].comp_size);
 
@@ -73,50 +106,13 @@ int wrapper_MPI_Isend( void *buf, int count, MPI_Datatype type, int dest,
 					pair[index].comp_size = comp_size;
 					pair[index].ready = 1;
 					pair[index].thread = 0;
-				}
-#endif
-				if(pair[index].ready == 1 && pair[index].comp_size != 0){
-					//printf("%d send index %d comp_size %d type_size %d\n",
-					//		rank, index, pair[index].comp_size, type_size);
-
-#if 0
-					printf("%d send index %d comp_size %d type_size %d pair_size %d comp_time %.3f send_time %.3f last_fault %.9f difference %.9f gap %.9f thread %d faults %d\n",
-							rank, index, pair[index].comp_size, type_size, pair_size,
-							pair[index].comp_time, pair[index].send_time, 
-							pair[index].last_fault,
-							pair[index].send_time - pair[index].comp_time,
-							pair[index].send_time - pair[index].last_fault,
-							pair[index].thread, pair[index].faults);
-#endif
-
-					//if(rank == 0)
-					printf("rank %d send i %d pair_size %d\n", rank, index, pair_size);
-
+					//printf("rank %d send i %d pair_size %d\n", rank, index, pair_size);
 					int comp_ret = MPI_Isend(pair[index].comp_addr, pair[index].comp_size, MPI_BYTE,
 							dest, tag, comm, request);
-					pair[index].faults = 0;
 					pthread_mutex_unlock(&(pair[index].pair_lock));
+
 					return comp_ret;
 				}
-#if 0	
-				else if(pair[index].ready != 1){
-					pair[index].ready == 1;
-					int comp_size = compress_lz4_buffer(pair[index].isend_addr, pair[index].isend_size,
-							pair[index].comp_addr, pair[index].comp_size);
-
-					if(comp_size < pair[index].isend_size && comp_size != 0){
-						pair[index].comp_size = comp_size;
-						pair[index].ready = 1;
-						pair[index].thread = 0;
-						printf("rank %d send i %d pair_size %d\n", rank, index, pair_size);
-						int comp_ret = MPI_Isend(pair[index].comp_addr, pair[index].comp_size, MPI_BYTE,
-								dest, tag, comm, request);
-						pthread_mutex_unlock(&(pair[index].pair_lock));
-
-						return comp_ret;
-					}
-				}
-#endif
 			}
 			pthread_mutex_unlock(&(pair[index].pair_lock));
 		}
@@ -179,14 +175,14 @@ int wrapper_MPI_Waitall( int count, MPI_Request array_of_requests[],
 	for(int i = 0; i < pair_size; i++){
 		pthread_mutex_unlock(&(pair[i].pair_lock));
 	}
-	
+
 	return MPI_Waitall(count, array_of_requests, array_of_statuses);
 }
 
 int wrapper_MPI_Init_thread( int *argc, char ***argv, int required, int *provided )
 {
 	int ret = MPI_Init_thread( argc, argv, required, provided );
-	
+
 	// init LRU cache and register list and pair list
 	pthread_mutex_lock(&cache_lock);
 	cache = create_cache(100);
@@ -196,7 +192,7 @@ int wrapper_MPI_Init_thread( int *argc, char ***argv, int required, int *provide
 
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
+
 	fargs = (struct fault_handler_args*)malloc(sizeof(struct fault_handler_args));
 	fargs->uffd = syscall(__NR_userfaultfd, O_CLOEXEC | O_NONBLOCK);
 	fargs->rank = rank;
@@ -205,7 +201,7 @@ int wrapper_MPI_Init_thread( int *argc, char ***argv, int required, int *provide
 	uffdio_api.api = UFFD_API;
 	uffdio_api.features = UFFD_FEATURE_PAGEFAULT_FLAG_WP;
 	assert(ioctl(fargs->uffd, UFFDIO_API, &uffdio_api) != -1);
-	
+
 	pthread_t uffd_thread;
 	assert(pthread_create(&uffd_thread, NULL, handler, (void*)fargs) == 0);
 
@@ -217,30 +213,30 @@ int wrapper_MPI_Init_thread( int *argc, char ***argv, int required, int *provide
 	if (result != 0) {
 		perror("Error setting thread affinity");
 	}
-	
+
 	pthread_t compression_thread1, compression_thread2;
 	comp_thread_args *arg1 = (comp_thread_args*)malloc(sizeof(comp_thread_args));
 	arg1->tn = 0;
 	arg1->total = 2;
 	arg1->rank = rank;
-	
+
 	comp_thread_args *arg2 = (comp_thread_args*)malloc(sizeof(comp_thread_args));
 	arg2->tn = 1;
 	arg2->total = 2;
 	arg2->rank = rank;
 
 	assert(pthread_create(&compression_thread1, NULL, starts_async_compression, (void*)arg1) == 0);
-//	assert(pthread_create(&compression_thread2, NULL, starts_async_compression, (void*)arg2) == 0);
-	
+	//assert(pthread_create(&compression_thread2, NULL, starts_async_compression, (void*)arg2) == 0);
+
 	cpu_set_t cpuset_compression1, cpuset_compression2, cpuset_compression3;
 	CPU_ZERO(&cpuset_compression1);
 	CPU_SET(rank + 8, &cpuset_compression1);
-	
-//	CPU_ZERO(&cpuset_compression2);
-//	CPU_SET(rank + 24, &cpuset_compression2);
-	
+
+	//CPU_ZERO(&cpuset_compression2);
+	//CPU_SET(rank + 24, &cpuset_compression2);
+
 	assert(pthread_setaffinity_np(compression_thread1, sizeof(cpu_set_t), &cpuset_compression1) == 0);
-//	assert(pthread_setaffinity_np(compression_thread2, sizeof(cpu_set_t), &cpuset_compression2) == 0);
+	//assert(pthread_setaffinity_np(compression_thread2, sizeof(cpu_set_t), &cpuset_compression2) == 0);
 
 	return ret;
 }
